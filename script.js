bc2e08364d0b1135edd03150783a67005288674c
Vinhv1 <vinhtrnh@gmail.com>
2023-10-17 19:02:28 +0300

    created a 16x16 grid with mouseover generating random colors

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const gridContainer = document.getElementById("gridContainer");
 
-for (let i = 0; i < 16; i++)
- {
-    for (let j = 0; j < 16; j++)
-    {
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 18; j++) {
+
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
-        gridItem.addEventListener("mouseover", () => {
-            gridItem.style.backgroundColor = "black"; // Change color on hover
+        gridItem.addEventListener("mouseover", () =>{
+            
+            gridItem.style.backgroundColor = getRandomColor();
         });
         gridContainer.appendChild(gridItem);
     }
+}
+
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i ++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
